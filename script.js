f7b5c077a3ad2d87d0193623485dd13e63c32d45
thanhphamphuong <40049174+ThanhPhamPhuong@users.noreply.github.com>
2024-02-04 17:14:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
-function sendData() {
-    // Get the input value from the HTML form
-    var inputData = document.getElementById('data.link').value;
+const CLIENT_ID = '417844596255-7rtlq3qhv9dm2cequp31003b4vdg2qkg.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyDUOnX3h1S21t3IpzvOwG1XXaltzb3sbR4';
+const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'];
+const SCOPES = 'https://www.googleapis.com/auth/drive.file';
 
-    // Call a function from the JavaScript file to handle the data
-    handleData(inputData);
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
 }
 
-// Function to handle the received data
-function handleData(data) {
-    alert('Received data in JS file: ' + data);
+function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    }).then(function () {
+        console.log('Google Drive API client initialized');
+    });
+}
 
-    // You can perform additional actions with the data here
+function uploadFile() {
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+
+    if (file) {
+        const metadata = {
+            'name': file.name,
+            'mimeType': file.type
+        };
+
+        const form = new FormData();
+        form.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
+        form.append('file', file);
+
+        gapi.client.drive.files.create({
+            resource: metadata,
+            media: {
+                mimeType: file.type,
+                body: form
+            }
+        }).then(function (response) {
+            console.log('File uploaded successfully:', response);
+            alert('File uploaded successfully!');
+        }, function (error) {
+            console.error('Error uploading file:', error);
+            alert('Error uploading file.');
+        });
+    } else {
+        alert('Please select a file to upload.');
+    }
 }
