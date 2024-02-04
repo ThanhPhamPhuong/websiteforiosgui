805ccc6b7ae53a65e286bd01030bd9691d6845f4
thanhphamphuong <40049174+ThanhPhamPhuong@users.noreply.github.com>
2024-02-04 16:33:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function sendData() {
+    // Get the input value from the HTML form
+    var inputData = document.getElementById('data.link').value;
 
+    // Call a function from the JavaScript file to handle the data
+    handleData(inputData);
+}
+
+// Function to handle the received data
+function handleData(data) {
+    alert('Received data in JS file: ' + data);
+
+    // You can perform additional actions with the data here
+}
